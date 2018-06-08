60c32ccbc02197f0a4b0f60025d416bcfced8ca1
DimaGashko <dgashko@mail.ua>
2018-06-08 16:51:53 +0300

    addSelected

--- angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -15,9 +15,14 @@ export class HeroesComponent implements OnInit {
   }
 
   heroes = HEROES;
+  selectedHero: Hero;
 
   constructor() { 
     
+  }
+
+  onSelect(hero: Hero): void { 
+    this.selectedHero = hero;
   }
 
   ngOnInit() {
