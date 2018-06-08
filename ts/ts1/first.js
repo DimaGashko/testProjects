1d690e74fddb8954168d7239815331acb5cce430
DimaGashko <dgashko@mail.ua>
2018-06-08 11:17:25 +0300

    edit

---
+++ ts/ts1/first.js
@@ -0,0 +1,10 @@
+var a = 5;
+var mes = "asdf " + a;
+var b = true;
+console.log(mes);
+var arr = [1, 2, 3, '4'];
+var Game = /** @class */ (function () {
+    function Game() {
+    }
+    return Game;
+}());
--- ts/ts1/first.ts
+++ ts/ts1/first.ts
@@ -1,1 +1,19 @@
-console.log(123);
+var a: number = 5;
+const mes: string | number = `asdf ${a}`;
+let b: boolean = true;
+console.log(mes);
+
+var arr: Array<number|string> = [1, 2, 3, '4'];
+
+interface GameInterface { 
+   name: string;
+   size: number;
+}
+
+class Game implements GameInterface {
+   name: string = "asdf";
+
+   constructor(public size: number = 5) { 
+
+   }
+}
