7903963c9e9713d53334596df181b19f20e8a2bc
DimaGashko <dgashko@mail.ua>
2018-06-08 12:57:31 +0300

    edit1

--- angular/my-app/src/app/app.component.ts
+++ angular/my-app/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number = 0;
+  name: string = "Hidden";
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'WindStorm',
+  }
 }
