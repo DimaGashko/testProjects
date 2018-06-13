644c4b3a6b3ef369369c827b64d6707eaa850a36
DimaGashko <dgashko@mail.ua>
2018-06-13 20:49:42 +0300

    edit

--- angular/testttt/src/app/app.component.ts
+++ angular/testttt/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
    selector: 'app-root',
-   template: 'ff'//'<h1>{{appName}} - {{appDescription}}</h1>'
+   template: '<h1>{{appName}} - {{appDescription}}</h1>'
    //templateUrl: './app.component.html',
    //styleUrls: ['./app.component.css']
 })
