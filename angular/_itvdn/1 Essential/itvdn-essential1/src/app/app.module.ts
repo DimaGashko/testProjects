84bfc9fd84d20e1afc207575068b287444596663
DimaGashko <dgashko@mail.ua>
2018-06-22 11:46:53 +0300

    addBaseRouter

--- angular/_itvdn/1 Essential/itvdn-essential1/src/app/app.module.ts
+++ angular/_itvdn/1 Essential/itvdn-essential1/src/app/app.module.ts
@@ -1,19 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { List1Component } from './list1/list1.component';
+import { List2Component } from './list2/list2.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    List1Component,
+    List2Component,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot([
+      { path: 'list1', component: List1Component },
+      { path: 'list2', component: List2Component },
+      { path: '', redirectTo: 'list1', pathMatch: 'full' },
+    ])
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
