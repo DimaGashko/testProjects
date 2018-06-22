577e557986b4a247928d61cba579fad75d62efc7
DimaGashko <dgashko@mail.ua>
2018-06-22 08:00:17 +0300

    edit

--- angular/_itvdn/1 Essential/itvdn-essential1/src/app/app.module.ts
+++ angular/_itvdn/1 Essential/itvdn-essential1/src/app/app.module.ts
@@ -4,12 +4,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloListComponent } from './hello-list/hello-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloListComponent
   ],
   imports: [
     BrowserModule,
