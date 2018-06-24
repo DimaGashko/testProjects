b30700545d97f668473e7db2e9b1a77d5ed4dfc2
DimaGashko <dgashko@mail.ua>
2018-06-24 08:01:25 +0300

    bindin

--- angular/_itvdn/1 Essential/homework2/src/app/app.module.ts
+++ angular/_itvdn/1 Essential/homework2/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }    from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MyTableComponent } from './my-table/my-table.component';
@@ -12,7 +13,8 @@ import { ProductComponent } from './product/product.component';
     ProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
