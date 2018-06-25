306552109a40117fc15285a24019a8dee4fe2756
DimaGashko <dgashko@mail.ua>
2018-06-25 07:28:23 +0300

    edit

--- angular/_itvdn/1 Essential/homework2-3/src/app/Product.ts
+++ angular/_itvdn/1 Essential/homework2-3/src/app/Product.ts
@@ -2,4 +2,5 @@ export class Product {
    id: number;
    name: string;
    price: number;
+   group: 'group1' | 'group2' | 'group3';
 }
--- angular/_itvdn/1 Essential/homework2-3/src/app/products-db.ts
+++ angular/_itvdn/1 Essential/homework2-3/src/app/products-db.ts
@@ -1,14 +1,14 @@
 import { Product } from './product'
 
 export const Products: Product[] = [
-    { id: 1, name: "product 1", price : 100 },
-    { id: 2, name: "product 2", price : 200 },
-    { id: 3, name: "product 3", price : 800 },
-    { id: 4, name: "product 4", price : 400 },
-    { id: 5, name: "product 5", price : 500 },
-    { id: 6, name: "product 6", price : 450 },
-    { id: 7, name: "product 7", price : 300 },
-    { id: 8, name: "product 8", price : 800 },
-    { id: 9, name: "product 9", price : 990 },
-    { id: 10, name: "product 10", price: 1000 },
+    { id: 1, name: "product 1", price: 100, group: 'group1' },
+    { id: 2, name: "product 2", price : 200, group: 'group2' },
+    { id: 3, name: "product 3", price : 800, group: 'group3' },
+    { id: 4, name: "product 4", price : 400, group: 'group1' },
+    { id: 5, name: "product 5", price : 500, group: 'group2' },
+    { id: 6, name: "product 6", price : 450, group: 'group3' },
+    { id: 7, name: "product 7", price : 300, group: 'group1' },
+    { id: 8, name: "product 8", price : 800, group: 'group2' },
+    { id: 9, name: "product 9", price : 990, group: 'group2' },
+    { id: 10, name: "product 10", price: 1000, group: 'group1' }, 
 ]; 
