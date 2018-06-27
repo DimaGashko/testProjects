a992a43a7aadb4c61a5efd6115699c04cca049eb
DimaGashko <dgashko@mail.ua>
2018-06-27 06:57:11 +0300

    base

--- angular/_itvdn/1 Essential/routerTests/src/app/app.module.ts
+++ angular/_itvdn/1 Essential/routerTests/src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { InfoComponent } from './info/info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { 
+        path: '',
+        redirectTo: 'main',
+        pathMatch: 'full',
+      },
+      {
+        path: 'main',
+        component: MainComponent,
+      },
+      {
+        path: 'info/:val',
+        component: InfoComponent, 
+      }
+    ]),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
