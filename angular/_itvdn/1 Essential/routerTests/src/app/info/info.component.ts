e4c785b834927eedb0a63b09c8fb0411dc43fddf
DimaGashko <dgashko@mail.ua>
2018-06-27 07:24:38 +0300

    baseRouters

--- angular/_itvdn/1 Essential/routerTests/src/app/info/info.component.ts
+++ angular/_itvdn/1 Essential/routerTests/src/app/info/info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-info',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./info.component.css']
 })
 export class InfoComponent implements OnInit {
+  val: string = '';
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+  ) { }
 
   ngOnInit() {
+    this.activatedRoute.params.forEach((params: Params) => {
+      const val = params.val;
+      this.val = val;
+    });
   }
 
 }
