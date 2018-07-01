a8193e5b27ecb73d5dbefee60b8e9584de1df541
DimaGashko <dgashko@mail.ua>
2018-07-01 15:49:39 +0300

    addOnEnter

--- angular/_itvdn/2 Advanced/observable/src/app/app.component.ts
+++ angular/_itvdn/2 Advanced/observable/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, EventEmitter } from '@angular/core';
+import { Observable, from } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,28 +8,32 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  values: string = '';
+  obs: Observable<number>;
+
+  event: EventEmitter<number[]> = new EventEmitter();
+
+  get data(): number[] {
+    return this.values.trim().split('')
+      .map(n => +n).filter(x => isNaN(x) === false);
+  }
   
   ngOnInit(): void {
-    console.clear();
-    
-    var obs = new Observable((observer) => {
-      var timer = setInterval(() => {
-        observer.next('Hi');
-      }, 500);
+    //const obs: Observable<number> = from([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+    this.obs = new Observable((observer => { 
+      this.event.subscribe(numbers => { 
+        numbers.forEach(n => observer.next(n));
+      });
+    }));
 
-      setTimeout(() => {
-        observer.complete();
-      }, 5000);
 
-      return () => {
-        clearInterval(timer);
-      }
-    });
+    this.obs.pipe(filter(x => x % 2 === 0))
+      .pipe(map(x => x + '.'))
+      .forEach(x => console.log(x));
+  }
 
-    obs.subscribe(
-      (next) => console.log(next),
-      () => console.log('error'),
-      () => console.log('complete')
-    );
+  next() {
+    this.event.emit(this.data);
+    this.values = '';
   }
 }
