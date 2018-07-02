15832ef4f4af504fe382ff3fbf0f9be95f47ea18
DimaGashko <dgashko@mail.ua>
2018-07-02 07:04:39 +0300

    changeColorOnClick

--- angular/_itvdn/2 Advanced/directives/src/app/event.directive.ts
+++ angular/_itvdn/2 Advanced/directives/src/app/event.directive.ts
@@ -1,10 +1,35 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer2, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appEvent]'
 })
 export class EventDirective {
+  colors: string[] = ['#000', '#fff', 'red', 'blue', 'green', 'yellow', 'pink'];
+  curColorIndex: number = 0;
 
-  constructor() { }
+  get curColor():string {
+    return this.colors[this.curColorIndex];
+  }
+
+  get el(): any {
+    return this.elementRef.nativeElement;
+  }
+
+  @HostListener('click') onclick() {
+    this.updateColor();
+  }
+
+  constructor(public elementRef: ElementRef, public renderer: Renderer2) {
+    this.updateColor();
+  }
+
+  updateColor() {
+    this.renderer.setStyle(this.el, 'color', this.curColor);
+    this.updateCurColor();
+  }
+
+  updateCurColor() {
+    this.curColorIndex = (this.curColorIndex + 1) % this.colors.length;
+  }
 
 }
