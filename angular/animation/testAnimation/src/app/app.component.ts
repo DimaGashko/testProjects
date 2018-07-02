537bab223843ca089d52e2fbc6f17b459bc6fe99
DimaGashko <dgashko@mail.ua>
2018-07-02 12:37:39 +0300

    edit

--- angular/animation/testAnimation/src/app/app.component.ts
+++ angular/animation/testAnimation/src/app/app.component.ts
@@ -13,7 +13,11 @@ import { trigger, state, style, transition, animate, keyframes } from '@angular/
       state('large', style({
         transform: 'scale(1.2)',
       })),
-      transition('small <=> large', animate('300ms'))
+      transition('small <=> large', animate('300ms', keyframes([
+        style({ opacity: 0, transform: 'translateY(-75%)', offset: 0 }),
+        style({ opacity: 1, transform: 'translateY(-35px)', offset: .5 }),
+        style({ opacity: 1, transform: 'translateY(0)', offset: 1 }),
+      ])))
     ]),
   ],
 })
