457927573805c0de8cbc40a2f3280e18f726cfa7
DimaGashko <dgashko@mail.ua>
2018-07-02 12:32:18 +0300

    baseAnimation

---
+++ angular/animation/testAnimation/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('myAnimation', [
+      state('small', style({
+        transform: 'scale(1)',
+      })),
+      state('large', style({
+        transform: 'scale(1.2)',
+      })),
+      transition('small <=> large', animate('300ms'))
+    ]),
+  ],
+})
+export class AppComponent {
+  state: string = 'small';
+
+  animateMe() {
+    this.state = (this.state === 'large') ? 'small' : 'large';
+  }
+}
