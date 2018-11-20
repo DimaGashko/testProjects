0cf60b63696f002e968cfe5c36c9a8b923d5d82d
DimaGashko <dgashko@mail.ua>
2018-11-21 01:27:44 +0200

    baseDebug

--- cpp/CppInVSCodeTests/Test1/main.cpp
+++ cpp/CppInVSCodeTests/Test1/main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
+   int a = 5, b = 6, c = 8;
+  
    cout << "Hello!" << endl;
    cout << "Who are you now?" << endl;
+
+   vector<int> aa;
+   aa.push_back(4);
+   aa.push_back(44);
 
    system("pause");
    return 0;
