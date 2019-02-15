1d6d32d818dba8bae24e0f655b16babb1c387c2c
DimaGashko <dgashko@mail.ua>
2019-02-15 21:51:05 +0200

    initFancyTabs

--- 1. Custom Elements Learn/src/pages/page2/FancyTabs.ts
+++ 1. Custom Elements Learn/src/pages/page2/FancyTabs.ts
@@ -1,7 +1,24 @@
 export default class FancyTabs extends HTMLElement {
+   private shadow: ShadowRoot = null;
+
    constructor() {
       super();
 
+      this.initShadow();
+
+      this.onclick = () => console.log('ASDF'); 
+      
+   }
+
+   private initShadow() {
+      this.shadow = this.attachShadow({ mode: 'open' });
+
+      this.shadow.innerHTML = `
+         <style>#tabs { }</style> <!-- styles are scoped to fancy-tabs! -->
+         <div id="tabs">...</div>
+         <div id="panels">...</div>
+         <slot></slot>
+      `;
    }
 }
 
--- 1. Custom Elements Learn/src/pages/page2/page2.ts
+++ 1. Custom Elements Learn/src/pages/page2/page2.ts
@@ -1,2 +1,4 @@
 import '../_templates/basePage/basePage';
 import './page2.sass';
+
+import './FancyTabs';
