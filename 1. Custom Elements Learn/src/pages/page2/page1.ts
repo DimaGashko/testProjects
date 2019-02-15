cbc89f3f13041c4ece9522b79cd89b77b2a68fb8
DimaGashko <dgashko@mail.ua>
2019-02-15 20:07:54 +0200

    edit

--- 1. Custom Elements Learn/src/pages/page2/page1.ts
+++ 1. Custom Elements Learn/src/pages/page2/page1.ts
@@ -1,31 +1,6 @@
 import '../_templates/basePage/basePage';
 import './page1.sass';
 
-// See https://html.spec.whatwg.org/multipage/indices.html#element-interfaces
-// for the list of other DOM interfaces.
-class FancyButton extends HTMLButtonElement {
-   constructor() {
-      super(); // always call super() first in the constructor.
-      this.addEventListener('click', e => this.drawRipple(e.offsetX, e.offsetY));
-   }
-
-   // Material design ripple animation.
-   drawRipple(x: number, y: number) {
-      let div = document.createElement('div');
-      div.classList.add('ripple');
-      this.appendChild(div);
-      div.style.top = `${y - div.clientHeight / 2}px`;
-      div.style.left = `${x - div.clientWidth / 2}px`;
-      div.style.backgroundColor = 'currentColor';
-      div.classList.add('run');
-      div.addEventListener('transitionend', e => div.remove());
-      
-   }
-}
-
-customElements.define('fancy-button', FancyButton, { extends: 'button' });
-
-
 const tmpl = <HTMLTemplateElement>document.querySelector('#x-foo-from-template');
 
 customElements.define('x-foo-shadow-dom', class extends HTMLElement {
