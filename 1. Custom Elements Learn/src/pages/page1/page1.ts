e6ae532d20f55dd8df6c6e515e5aae2810091b66
DimaGashko <dgashko@mail.ua>
2019-02-15 18:16:58 +0200

    init

--- 1. Custom Elements Learn/src/pages/page1/page1.ts
+++ 1. Custom Elements Learn/src/pages/page1/page1.ts
@@ -1,4 +1,45 @@
 import '../_templates/basePage/basePage';
 import './page1.sass';
 
-console.log('Page1');
+class AppDrawer extends HTMLElement {
+   public get disabled(): boolean {
+      return this.hasAttribute('disabled');
+   }
+
+   public set disabled(val: boolean) {
+      if (val) {
+         this.setAttribute('disabled', '');
+
+      } else {
+         this.removeAttribute('disabled');
+
+      }
+      
+   }
+   
+   constructor() {
+      super();
+
+      this.init();
+   }
+
+   private init() {
+      this.initEvent();
+   }
+
+   private initEvent() {
+      this.addEventListener('click', () => {
+         this.sayHello();
+      });
+   }
+
+   private sayHello() {
+      if (this.disabled) return;
+
+      console.log("Hello, I'm a AppDrawer");
+      
+   }
+
+}
+
+window.customElements.define('app-drawer', AppDrawer);
