ce7b3b738b844898a5ce6034cfdb1774a4e5a380
DimaGashko <dgashko@mail.ua>
2019-02-17 01:10:38 +0200

    appComponent

--- 1. Custom Elements Learn/src/pages/page4/components/app/app-component.ts
+++ 1. Custom Elements Learn/src/pages/page4/components/app/app-component.ts
@@ -3,10 +3,10 @@ import CustomElement from "../../../../components/CustomElement/CustomElement";
 @CustomElement({
    selector: 'x-app',
    template: require('./app-component.pug'),
-   style: require('./app-component.sass'),
+   style: require('./app-component.string.sass'),
    useShadow: true,
 })
-class AppComponent extends HTMLElement {
+export default class AppComponent extends HTMLElement {
    constructor() { 
       super();
 
