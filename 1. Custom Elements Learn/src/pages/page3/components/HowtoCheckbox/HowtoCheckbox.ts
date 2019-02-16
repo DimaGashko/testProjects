0d30dfb3134008dad2d6dd84bb8faf7b8baa6f20
DimaGashko <dgashko@mail.ua>
2019-02-16 22:19:56 +0200

    edit

--- 1. Custom Elements Learn/src/pages/page3/components/HowtoCheckbox/HowtoCheckbox.ts
+++ 1. Custom Elements Learn/src/pages/page3/components/HowtoCheckbox/HowtoCheckbox.ts
@@ -1,8 +1,10 @@
 import * as templateStr from './template.pug';
-import * as style from './style.sass';
-console.log(style);
+import * as _style from './style.sass';
+const style = _style.toString();
 const template = document.createElement('template');
 template.innerHTML = `<style>${style}</style>${templateStr}`;
+console.log(`<style>${style}</style>${templateStr}`);
+
 
 export default class HowtoCheckbox extends HTMLElement {
    private template = template;
