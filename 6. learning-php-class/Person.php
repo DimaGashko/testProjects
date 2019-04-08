4405ede97d9d9909df49aa03f0515fcf4be235d0
DimaGashko <dimagashko@gmail.com>
2019-04-09 00:11:13 +0300

    base

---
+++ 6. learning-php-class/Person.php
@@ -0,0 +1,17 @@
+<?php
+
+spl_autoload_register(function($class_name) {
+    require $class_name . '.php';
+});
+
+class Person {
+    public $firstName;
+    public $lastName;
+    public $age;
+
+    function __construct($firstName, $lastName, $age) {
+        $this->firstName = $firstName;
+        $this->$lastName = $lastName;
+        $this->age = $age; 
+    }
+}
