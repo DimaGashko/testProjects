07b32ba4758532c9b011cf628edf49610f403ae3
DimaGashko <dimagashko@gmail.com>
2019-04-09 14:42:13 +0300

    edit

---
+++ 4. php basics/1. phpBasics/2/1.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Test php</title>
+</head>
+<body>
+    <h1><?= '1.php' . (2 * 3.5) ?></h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, odio earum ex ullam perspiciatis error ratione eaque, aliquid consequatur possimus a itaque provident! Dolor dolores alias velit aliquid iusto magni!</p>
+</body>
+</html>
