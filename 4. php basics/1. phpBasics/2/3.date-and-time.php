edc2e482a5f6d3f7d861ac7f6c1176a2fa74ea29
DimaGashko <dimagashko@gmail.com>
2019-04-09 18:14:31 +0300

    baseDate

--- 4. php basics/1. phpBasics/2/3.date-and-time.php
+++ 4. php basics/1. phpBasics/2/3.date-and-time.php
@@ -1,2 +1,17 @@
 <?php
 
+$date = new DateTime();
+printDate($date);
+
+$date->add(new DateInterval('P2Y2DT2H'));
+printDate($date);
+
+$date->modify('+1 day');
+printDate($date);
+
+$date->modify('-1 day');
+printDate($date);
+
+function printDate($date) {
+    echo $date->format('Y-m-d h-i-s'), PHP_EOL;
+}
