7c6e012fb6988a2abcdee207d45b177c84de0795
DimaGashko <dimagashko@gmail.com>
2019-05-29 19:50:01 +0300

    basicGlobals

--- 4. php basics/5. week8-9/globals/1.php
+++ 4. php basics/5. week8-9/globals/1.php
@@ -2,19 +2,23 @@
 
 $x = "I'm an X";
 
-function hello() {
+function hello()
+{
     $y = "I'm an Y";
 
     echo $GLOBALS["x"];
 }
 
-var_dump($_GET);
-
 hello();
 
 ?>
 
-<form method="POST" action="hello.php">
+<form method="POST">
     <input type="hidden" name="name" value="value">
     <button type="submit">JUST PRESS ME!</button>
 </form>
+
+<pre><?= var_dump($_GET); ?></pre>
+<pre><?= var_dump($_POST); ?></pre>
+<pre><?= var_dump($_SERVER); ?></pre>
+<pre><?= var_dump($_SELF); ?></pre>
