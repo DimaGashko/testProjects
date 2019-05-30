b18e733031055bb4c1450d36805d27e91b2ce675
DimaGashko <dimagashko@gmail.com>
2019-05-30 15:30:26 +0300

    edit

--- 6. vue/1. base tutorial/main.js
+++ 6. vue/1. base tutorial/main.js
@@ -1,8 +1,12 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.esm.browser.js';
 
 var app = new Vue({
-    el: '.app',
+    el: '#app',
     data: {
         product: 'Socks',
-    }
+        image: './vmSocks-blue-onWhite.jpg',
+        inventory: 10,
+    },
 });
+
+window.app = app;
