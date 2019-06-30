1e5b20646498278eeea03fafa0302bc7ef09a581
DimaGashko <dimagashko@gmail.com>
2019-06-30 14:14:06 +0300

    loops

---
+++ 8.go/1/4.loops/2/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	n := 20
+
+	for i := 0; i <= n; i++ {
+		for j := 0; j <= n; j++ {
+			c := "   "
+
+			if (j%2 == 0 && i%2 != 0) || (j%2 != 0 && i%2 == 0) {
+				c = "***"
+			}
+
+			fmt.Print(c)
+		}
+
+		fmt.Println()
+
+	}
+
+}
